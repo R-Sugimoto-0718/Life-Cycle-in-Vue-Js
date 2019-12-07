2023ffd718a587b4aec6a2b1cf84143229d4f299
waspryo <wasp2123321@gmail.com>
2019-12-07 17:39:57 +0900

    VeforeCreated

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    
+    <student></student>
+    <employee></employee>
   </div>
 </template>
 
 <script>
+import Student from './components/Student'
+import Employee from './components/Employee'
 
 export default {
+  components: {
+    "student": Student,
+    "employee": Employee
+  },
  data() {
    return {
      
    }
  },
+ beforeCreate() {
+   alert('Before Create')
+ }
 }
 </script>
 
