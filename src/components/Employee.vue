9fbffbb0ef94633e8636b0375b2bf9b408f75379
waspryo <wasp2123321@gmail.com>
2019-12-07 17:36:47 +0900

    compile template

---
+++ src/components/Employee.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>
+      <h3>Employee: {{ messafe }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hi I am Employee Component'
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Student.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>
+      <h3>Student: {{ messafe }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hi I am Student Component'
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
